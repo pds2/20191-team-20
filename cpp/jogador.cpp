3d007b5f7059f7ca04a0aeab7a18bdaba24cbce2
ThiagoXX0 <thiago.a.campos@hotmail.com>
2019-05-25 13:55:17 -0300

    Update jogador.cpp

--- cpp/jogador.cpp
+++ cpp/jogador.cpp
@@ -24,7 +24,39 @@ void Jogador::adicionaCartaEmMao(vector<Carta> _cartas){
         for(int i=0;i<_cartas.size();i++){
             cartasEmMao.push_back(_cartas[i]);
         }
+        cout<<"Cartas adicionadas !";
+    }else{
+        cout<<" Não é possível adicionar nenhuma carta !";
     }
+}
+
+void Jogador::removeCartaEmMao(vector<Carta> _cartas){
+    if(_cartas.size() > 0 && cartasEmMao.size() > 0){
+        int posicoes[20],TAM=0;
+        // For para iterar em cima das cartas que serão removidas e buscá-las na mão do jogador.
+        for(int k=0;k<_cartas.size();k++){
+            for(int i=0;i<cartasEmMao.size();i++){
+                if(_cartas[k].getNaipe() == cartasEmMao[i].getNaipe() 
+                && _cartas[k].getCaracter() == cartasEmMao[i].getCaracter()){
+                    posicoes[TAM] = i;
+                    TAM++;
+                }
+            }
+        }
+        // for para acertar as posicoes a serem removidas a cada remoção.
+        for(int j=0;j<TAM;j++){
+            cartasEmMao.erase(cartasEmMao.begin()+posicoes[j]);
+            for(int l=j+1;l<TAM;l++){
+                if(posicoes[l] > posicoes[j]){
+                    posicoes[l] -= 1; 
+                }
+            }
+        }
+        cout<<" Carta(s) removida(s) com sucesso !";
+    }else{
+        cout<<"Não é possível remover nenhuma carta ou o jogador não possui nenhuma carta em mãos ! ";
+    }
+
 }
 
 
